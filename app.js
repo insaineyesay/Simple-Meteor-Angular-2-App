8c3a7f2cdee833c9ffaa6213fa699e82bd14e971
insaineyesay <insaineyesay@gmail.com>
2015-09-12 10:35:55 -0400

    refacted save functionality so that app doesn't save on ever character input

--- app.js
+++ app.js
@@ -37,7 +37,18 @@ if (Meteor.isClient) {
     }]);
     angular.module('measure').controller('FeelingDetailsCtrl', ['$scope', '$stateParams', '$meteor',
       function ($scope, $stateParams, $meteor) {
-        $scope.feeling = $meteor.object(Feelings, $stateParams.feelingId);
+        $scope.feeling = $meteor.object(Feelings, $stateParams.feelingId, false);
+        $scope.save = function () {
+          $scope.feeling.save()
+            .then(function (numberOfDocs) {
+              console.log('save success doc affected ', numberOfDocs);
+            }, function (error) {
+              console.log('save error', error);
+            });
+        };
+        $scope.reset = function () {
+          $scope.feeling.reset();
+        };
       }
     ]);
 }
