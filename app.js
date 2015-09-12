be69aae96e5d5545f78f4a467c40e0660e4e0980
insaineyesay <insaineyesay@gmail.com>
2015-09-12 00:05:35 -0400

    form included on home page to add new entries into the database.

--- app.js
+++ app.js
@@ -1,19 +1,25 @@
+Feelings = new Mongo.Collection("feelings");
+
 if (Meteor.isClient) {
-  angular.module('measure', ['angular-meteor']);
-  angular.module('measure').controller('MeasureCtrl', ['$scope', function ($scope) {
-    $scope.parties = [
-      {
-        'name': 'Dubstep-Free Zone',
-        'description': 'Can we please just for an evening not listen to dubstep.'
-      },
-      {
-        'name': 'All dubstep all the time',
-        'description': 'Get it on!'
-      },
-      {
-        'name': 'Savage lounging',
-        'description': 'Leisure suit required. And only fiercest manners.'
-      }
-    ];
-  }])
+    angular.module('measure', ['angular-meteor']);
+    angular.module('measure').controller('MeasureCtrl', ['$scope', '$meteor', function ($scope, $meteor) {
+      $scope.feelings = $meteor.collection(Feelings);
+  }]);
+}
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    if (Feelings.find().count() === 0) {
+      var feelings = [
+        {'title': 'Dubstep-Free Zone',
+          'description': 'Fast just got faster with Nexus S.'},
+        {'title': 'All dubstep all the time',
+          'description': 'Get it on!'},
+        {'title': 'Savage lounging',
+          'description': 'Leisure suit required. And only fiercest manners.'}
+      ];
+      for (var i = 0; i < feelings.length; i++)
+        Feelings.insert(feelings[i]);
+    }
+  });
 }
