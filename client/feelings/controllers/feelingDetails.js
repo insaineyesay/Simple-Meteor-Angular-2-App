9f9f192a1d5d33bc2b3882385956b1984e874ec6
insaineyesay <insaineyesay@gmail.com>
2015-09-12 20:18:47 -0400

    improved privacy. no views unless logged in. No creating unless logged in.

--- client/feelings/controllers/feelingDetails.js
+++ client/feelings/controllers/feelingDetails.js
@@ -1,6 +1,7 @@
 angular.module('measure').controller('FeelingDetailsCtrl', ['$scope', '$stateParams', '$meteor',
   function ($scope, $stateParams, $meteor) {
-    $scope.feeling = $meteor.object(Feelings, $stateParams.feelingId, false);
+    $scope.feeling = $meteor.object(Feelings, $stateParams.feelingId).subscribe('feelings');
+    $scope.users = $meteor.collection(Meteor.users, false).subscribe('users');
     $scope.save = function () {
       $scope.feeling.save()
         .then(function (numberOfDocs) {
